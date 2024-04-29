d11053e4e071b506c9a6bc3579b30956398f11bd
Mouli1704 <168407788+Mouli1704@users.noreply.github.com>
2024-04-29 13:20:10 +0530

    Add files via upload

---
+++ HandsON/Module.py
@@ -0,0 +1,11 @@
+def addition(a,b):
+    return f"The sum of the numbers is: {a+b}"
+
+def subtraction(a,b):
+    return f"The difference of the two numbers is: {abs((a-b))}"
+
+def multiplication(a,b):
+    return f"The product of the numbers {a , b} is: {a*b}"
+
+def division(a,b):
+    return f"The quotient when {a} divided by {b} is: {a/b}"
